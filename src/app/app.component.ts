26f8c4d791646d8f6e1608bd7bf2bd8ffe530ab0
Sergej B <sergej_bardin@yahoo.de>
2018-10-25 15:58:55 +0200

    added inital fw, assets and components: - Events and Movies with API - Banner (dummy) - Filter with datepicker (dummy) - Logo and dummy pics - UIKIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare var UIkit: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Outgofox-Angular5';
+  showAlert(): void {
+    UIkit.modal.alert('UIkit alert!');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { BannerComponent } from './banner/banner.component';
+import { FilterComponent } from './filter/filter.component';
+import { EventsComponent } from './events/events.component';
+import {AngularDateTimePickerModule} from 'angular2-datetimepicker';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    BannerComponent,
+    FilterComponent,
+    EventsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AngularDateTimePickerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
